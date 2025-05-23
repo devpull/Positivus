d4841a06f5f3916d64dd187d74a75ad42957d3ca
devpull <devpull@yandex.ru>
2025-05-23 10:07:09 +0300

    Adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   // site: "https://positivustheme.vercel.app",
   integrations: [tailwind()],
   output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
 });
